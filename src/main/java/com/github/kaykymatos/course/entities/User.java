db51bc0800b4d20bf252c13770373e6e25026063
Kayky Matos Santana <kayky7277@gmail.com>
2025-03-16 15:59:51 -0300

    Order, Instant, ISO 8601, new order table

--- src/main/java/com/github/kaykymatos/course/entities/User.java
+++ src/main/java/com/github/kaykymatos/course/entities/User.java
@@ -3,6 +3,8 @@ package com.github.kaykymatos.course.entities;
 import jakarta.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -13,17 +15,20 @@ public class User  implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
 
     public User(Long id, String name, String email, String phone, String password) {
-        Id = id;
+        super();
+        this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
@@ -31,11 +36,11 @@ public class User  implements Serializable {
     }
 
     public Long getId() {
-        return Id;
+        return id;
     }
 
     public void setId(Long id) {
-        Id = id;
+        this.id = id;
     }
 
     public String getName() {
@@ -70,22 +75,26 @@ public class User  implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(Id, user.Id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(phone, user.phone) && Objects.equals(password, user.password);
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(phone, user.phone) && Objects.equals(password, user.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(Id, name, email, phone, password);
+        return Objects.hash(id, name, email, phone, password);
     }
 
     @Override
     public String toString() {
         return "User{" +
-                "Id=" + Id +
+                "Id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
                 ", phone='" + phone + '\'' +
