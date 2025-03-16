f631a18e8880c00b751ef6801a9cf67417b9393d
Kayky Matos Santana <kayky7277@gmail.com>
2025-03-16 15:11:12 -0300

    Service layer, component registration

---
+++ src/main/java/com/github/kaykymatos/course/services/UserService.java
@@ -0,0 +1,22 @@
+package com.github.kaykymatos.course.services;
+
+import com.github.kaykymatos.course.entities.User;
+import com.github.kaykymatos.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> fidAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
