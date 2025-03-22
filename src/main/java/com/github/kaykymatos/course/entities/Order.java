5b51aa4472169afbbd8ab234c737d36978159245
Kayky Matos Santana <kayky7277@gmail.com>
2025-03-22 17:17:44 -0300

    Subtotal & Total methods

--- src/main/java/com/github/kaykymatos/course/entities/Order.java
+++ src/main/java/com/github/kaykymatos/course/entities/Order.java
@@ -30,7 +30,7 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
-    @OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
 
     public Order() {
@@ -88,6 +88,15 @@ public class Order implements Serializable {
     public void setPayment(Payment payment) {
         this.payment = payment;
     }
+
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItem item : items) {
+            sum += item.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
