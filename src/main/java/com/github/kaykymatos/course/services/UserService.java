18e874c37fc882a0680d77e7fd9b1afae988b382
Kayky Matos Santana <kayky7277@gmail.com>
2025-03-22 23:58:24 -0300

    User insert

--- src/main/java/com/github/kaykymatos/course/services/UserService.java
+++ src/main/java/com/github/kaykymatos/course/services/UserService.java
@@ -19,4 +19,8 @@ public class UserService {
     public User findById(Long id) {
         return userRepository.findById(id).orElse(null);
     }
+
+    public User insert(User user) {
+        return userRepository.save(user);
+    }
 }
